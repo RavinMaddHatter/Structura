0c2093ffbc0b7c2e60aeed227fd7db60181aa891
DrAv0011 <drav0011@gmail.com>
2021-01-13 19:53:51 +0100

    Reverted changes to separate code changes from formatting

--- PackMaker.py
+++ PackMaker.py
@@ -1,7 +1,7 @@
 import armor_stand_class
 import structure_reader
 import animation_class
-from tkinter import StringVar, Button, Label, Entry, Tk
+from tkinter import StringVar, Button,Label,Entry,Tk
 from tkinter import filedialog
 import manifest
 from shutil import copyfile
@@ -9,80 +9,67 @@ import os
 from zipfile import ZipFile
 import glob
 
-
-def generate_pack(struct_name, pack_name):
+def generate_pack(struct_name,pack_name):
     manifest.export(pack_name)
-    struct2make = structure_reader.process_structure(struct_name)
-    armorstand = armor_stand_class.armorstand()
-    animation = animation_class.animations()
-    [xlen, ylen, zlen] = struct2make.get_size()
+    struct2make=structure_reader.process_structure(struct_name)
+    armorstand=armor_stand_class.armorstand()
+    animation=animation_class.animations()
+    [xlen,ylen,zlen]=struct2make.get_size()
     for y in range(ylen):
         armorstand.make_layer(y)
         animation.insert_layer(y)
         for x in range(xlen):
             for z in range(zlen):
-                block = struct2make.get_block(x, y, z)
-                armorstand.make_block(
-                    x, y, z, block["name"].replace("minecraft:", ""))
+                block=struct2make.get_block(x,y,z)
+                armorstand.make_block(x,y,z,block["name"].replace("minecraft:",""))
 
     armorstand.export(pack_name)
     animation.export(pack_name)
     copyfile("pack_icon.png", "{}/pack_icon.png".format(pack_name))
-    os.makedirs(os.path.dirname(
-        "{}/entity/armor_stand.entity.json".format(pack_name)), exist_ok=True)
-    copyfile("armor_stand.entity.json",
-             "{}/entity/armor_stand.entity.json".format(pack_name))
-
-    # Adds to zip file a modified armor stand geometry to enlarge the render area of the entity
-    copyfile("armor_stand.larger_render.geo.json",
-             "{}/models/entity/armor_stand.larger_render.geo.json".format(pack_name))
-
-    rc = "armor_stand.ghost_blocks.render_controllers.json"
-    rcpath = "{}/render_controllers/{}".format(pack_name, rc)
+    os.makedirs(os.path.dirname("{}/entity/armor_stand.entity.json".format(pack_name)), exist_ok=True)
+    copyfile("armor_stand.entity.json", "{}/entity/armor_stand.entity.json".format(pack_name))
+    rc="armor_stand.ghost_blocks.render_controllers.json"
+    rcpath="{}/render_controllers/{}".format(pack_name,rc)
     os.makedirs(os.path.dirname(rcpath))
-    copyfile(rc, rcpath)
-    file_paths = []
-    for directory, _, _ in os.walk(pack_name):
-        file_paths.extend(glob.glob(os.path.join(directory, "*.*")))
-    with ZipFile("{}.mcpack".format(pack_name), 'x') as zip:
-        # writing each file one by one
+    copyfile(rc,rcpath)
+    file_paths=[]
+    for directory,_,_ in os.walk(pack_name):
+        file_paths.extend(glob.glob(os.path.join(directory,"*.*")))
+    with ZipFile("{}.mcpack".format(pack_name),'x') as zip: 
+        # writing each file one by one 
         for file in file_paths:
             print(file)
             zip.write(file)
-
-
 def runFromGui():
     FileGUI
-    generate_pack(FileGUI.get(), packName.get())
-
-
+    generate_pack(FileGUI.get(),packName.get())
 def browseStruct():
-    FileGUI.set(filedialog.askopenfilename(filetypes=(
-        ("Structure File", "*.mcstructure *.MCSTRUCTURE"), )))
+    FileGUI.set(filedialog.askopenfilename(filetypes = (("Structure File", "*.mcstructure *.MCSTRUCTURE"), )))
+
 
 
 root = Tk()
 root.title("Bedrock Litematica Maker")
 
 
-FileGUI = StringVar()
-packName = StringVar()
-file_entry = Entry(root, textvariable=FileGUI)
-packName_entry = Entry(root, textvariable=packName)
-file_lb = Label(root, text="Structure file")
-packName_lb = Label(root, text="Pack Name")
-packButton = Button(root, text="Browse", command=browseStruct)
+FileGUI=StringVar()
+packName=StringVar()
+file_entry = Entry(root,textvariable=FileGUI)
+packName_entry = Entry(root,textvariable=packName)
+file_lb=Label(root, text="Structure file")
+packName_lb=Label(root, text="Pack Name")
+packButton=Button(root,text="Browse",command=browseStruct)
 
-saveButton = Button(root, text="Make Pack", command=runFromGui)
-r = 0
-file_lb.grid(row=r, column=0)
-file_entry.grid(row=r, column=1)
-packButton.grid(row=r, column=2)
-r += 1
-packName_lb.grid(row=r, column=0)
-packName_entry.grid(row=r, column=1)
-r += 1
-saveButton.grid(row=r, column=2)
+saveButton=Button(root,text="Make Pack",command=runFromGui)
+r=0
+file_lb.grid(row=r,column=0)
+file_entry.grid(row=r,column=1)
+packButton.grid(row=r,column=2)
+r+=1
+packName_lb.grid(row=r,column=0)
+packName_entry.grid(row=r,column=1)
+r+=1
+saveButton.grid(row=r,column=2)
 
 
-root.mainloop()
+root.mainloop() 
