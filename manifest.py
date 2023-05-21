782ecc8c3dbc48701e09c76831dc3a8f81f8c9e0
camerin <camerin.hahn@gmail.com>
2023-05-21 14:36:25 -0700

    fixed rails in world file

--- manifest.py
+++ manifest.py
@@ -7,7 +7,7 @@ def export(pack_name):
     manifest = {
         "format_version": 2,
         "header": {
-            "name": pack_name.replace("/tmp/",""),
+            "name": pack_name.replace("/tmp/","").replace("tmp/",""),
             "description": "Structura block overlay pack, created by  \u00a7o\u00a75DrAv0011\u00a7r, \u00a7o\u00a79 FondUnicycle\u00a7r and\u00a7o\u00a75 RavinMaddHatter\u00a7r",
             "uuid": str(uuid.uuid4()),
             "version": [
